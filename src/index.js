9caf8bf54722ca13f4bad38492709c514c6cc8f1
Martin Raedlinger <mr@formatlos.de>
2017-07-24 16:15:56 +0200

    Modal: add displayName

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import withBackdrop from './decorators/withBackdrop';
 import ModalBase from './components/ModalBase';
 
 const Modal = compose(withBackdrop, withSwipeToDismiss)(ModalBase);
+Modal.displayName = 'Modal';
 
 export default Modal;
 export { ModalBase, withBackdrop, withSwipeToDismiss };
